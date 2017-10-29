87b58376276a920c3896741695ec5ca323dd523b
Carmine Zaccagnino <zaccagnino@hotmail.it>
2017-10-29 15:45:15 +0100

    Passato da file statici HTML ad ejs

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ express = require('express');
 var app = express();
 app.use(express.static('public'));
 
+app.get('/',function(req,res) {
+  res.render("index.ejs");
+});
+
+app.get('/iniziamo',function(req,res) {
+  res.render("iniziamo.ejs");
+});
 
 app.listen(process.env.PORT, function () {
   console.log('Server avviato sulla porta '+process.env.PORT);
