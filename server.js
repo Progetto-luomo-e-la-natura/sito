644fd386b2e94a53611a5baf8face729f230c881
Carmine <carminezaccagnino@gmail.com>
2018-02-17 19:09:29 +0100

    Aggiunti middleware per migliore uso su server
    Signed-off-by: Carmine <carminezaccagnino@gmail.com>

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
+var helmet = require('helmet');
 app.use(express.static('public'));
-
+app.use(morgan('combined'));
+app.use(helmet());
 app.get('/',function(req,res) {
   res.render("index.ejs");
   res.end();
